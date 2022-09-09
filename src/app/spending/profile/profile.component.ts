dabcce70c2285f816812148ea1a51aed3eb9dab9
huy12199 <you@example.com>
2022-09-09 09:15:29 +0700

    add profile

--- src/app/spending/profile/profile.component.ts
+++ src/app/spending/profile/profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ScriptService} from "../../service/script.service";
+import {ProfileService} from "../../service/profile.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AppUser} from "../../model/AppUser";
+import {LoginserviceService} from "../../service/loginservice.service";
 
 @Component({
   selector: 'app-profile',
@@ -8,11 +12,22 @@ import {ScriptService} from "../../service/script.service";
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private script: ScriptService) { }
+user: AppUser = new AppUser(0,"","","","","","",0,0,"")
+
+  constructor(private script: ScriptService, private profileservice: ProfileService, private router: Router, private route: ActivatedRoute,private loginService: LoginserviceService) { }
 
   ngOnInit(): void {
     this.script.load('global','bootstrap-select','jquerymin','Chartbundle','lightgallery-all','custom','dlabnav','upimg').then(data => {
     }).catch(error => console.log(error));
+    this.showUser()
+  }
+
+  showUser(){
+    let id = this.loginService.getUserToken().id
+    this.profileservice.show(id).subscribe((data) => {
+      console.log(data)
+      this.user = data;
+    })
   }
 
 }
