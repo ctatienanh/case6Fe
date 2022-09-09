adaeb1e2aacc89883b9efb71e61d218263436927
huy12199 <you@example.com>
2022-09-09 09:27:53 +0700

    Revert "add profile"
    This reverts commit dabcce70c2285f816812148ea1a51aed3eb9dab9.

--- src/app/spending/profile/profile.component.ts
+++ src/app/spending/profile/profile.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {ScriptService} from "../../service/script.service";
-import {ProfileService} from "../../service/profile.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {AppUser} from "../../model/AppUser";
-import {LoginserviceService} from "../../service/loginservice.service";
 
 @Component({
   selector: 'app-profile',
@@ -12,22 +8,11 @@ import {LoginserviceService} from "../../service/loginservice.service";
 })
 export class ProfileComponent implements OnInit {
 
-user: AppUser = new AppUser(0,"","","","","","",0,0,"")
-
-  constructor(private script: ScriptService, private profileservice: ProfileService, private router: Router, private route: ActivatedRoute,private loginService: LoginserviceService) { }
+  constructor(private script: ScriptService) { }
 
   ngOnInit(): void {
     this.script.load('global','bootstrap-select','jquerymin','Chartbundle','lightgallery-all','custom','dlabnav','upimg').then(data => {
     }).catch(error => console.log(error));
-    this.showUser()
-  }
-
-  showUser(){
-    let id = this.loginService.getUserToken().id
-    this.profileservice.show(id).subscribe((data) => {
-      console.log(data)
-      this.user = data;
-    })
   }
 
 }
